a4837d355da48bd7cd322c093a39c2ec5780c01c
naveiniv <168838496+naveiniv@users.noreply.github.com>
2026-02-28 22:25:46 -0500

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,15 @@
+document.getElementById("safeBtn").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  if (tab && tab.id) {
+    if (tab.url && tab.url !== "chrome://newtab/") {
+      chrome.tabs.goBack(tab.id);
+    }
+  }
+
+  window.close();
+});
+
+document.getElementById("proceedBtn").addEventListener("click", () => {
+  window.close();
+});
